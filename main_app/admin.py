feb2de1c4bafe7b578aa483ce03133af22069bb1
mayuri chandrica <chandrikamayuri@gmail.com>
2022-07-31 14:23:06 +0530

    Initial commit

---
+++ main_app/admin.py
@@ -0,0 +1,18 @@
+
+# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
+# Register your models here.
+
+
+class UserModel(UserAdmin):
+    ordering = ('email',)
+
+
+admin.site.register(CustomUser, UserModel)
+admin.site.register(Staff)
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(Subject)
+admin.site.register(Session)
